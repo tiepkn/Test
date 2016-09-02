816a167d260357729effcb6fe35d6aec5adecca6
Tiep Nguyen Khac <nkhactiep2603@gmail.com>
2016-09-02 23:34:16 +0700

    trigger build

--- Jenkins/Jenkins/Controllers/AccountController.cs
+++ Jenkins/Jenkins/Controllers/AccountController.cs
@@ -18,6 +18,7 @@ namespace Jenkins.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+
         public AccountController()
         {
         }
