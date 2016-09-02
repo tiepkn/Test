cd871ebcf168de47843e6ae9abe18b96f57ff712
Tiep Nguyen Khac <nkhactiep2603@gmail.com>
2016-09-03 03:40:36 +0700

    fgdg

--- Jenkins/Jenkins/Controllers/AccountController.cs
+++ Jenkins/Jenkins/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ namespace Jenkins.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-		//hh gfh
+		//
         public AccountController()
         {
         }
