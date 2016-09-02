0225b1ed26c41f8146750a2e5e24eebd114f33d9
Tiep Nguyen Khac <nkhactiep2603@gmail.com>
2016-09-02 22:22:23 +0700

    hello test

---
+++ Jenkins/Jenkins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jenkins.Startup))]
+namespace Jenkins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
